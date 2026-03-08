da9f80df8082167afbcc404be3aa3110c4b447e5
Tian NIE <tiannie@device-55.home>
2026-03-08 02:33:15 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ def main():
             content = f.read()  # clear text content
             obj_content = f'blob {len(content)}\0{content}'.encode()  # content with header in bytecode
             hex_digest = hashlib.sha1(obj_content).hexdigest()
+            (Path('.git/objects') / hex_digest[:2]).mkdir(exist_ok=True)
             with open(Path('.git/objects') / hex_digest[:2] / hex_digest[2:] , "wb") as f_obj:
                 f_obj.write(zlib.compress(obj_content))
     else:
