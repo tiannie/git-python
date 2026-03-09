225b78567a1877f38930704414dee6994c0f09aa
Tian NIE <tiannie@device-55.home>
2026-03-09 03:36:05 +0100

    codecrafters submit [skip ci]

--- app/command/ls_tree.py
+++ app/command/ls_tree.py
@@ -1,5 +1,15 @@
+import hashlib
+import zlib
+from pathlib import Path
 from typing import List
 
 
 def ls_tree(sys_argv: List[str]) -> None:
-    print("ls_tree", sys_argv)
+    hex_digest = sys_argv[3]
+    with open(Path('.git/objects') / hex_digest[:2] / hex_digest[2:], "rb") as f:
+        content = zlib.decompress(f.read())  # clear text content
+        segments = content.split(b'\0')
+        for i, segment in enumerate(segments):
+            if i:
+                name = segment.split(b' ')[1]
+                print(name)
